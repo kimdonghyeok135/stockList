a44508a1fbb8d644527977af57d2dbc61a32bfef
김동혁 <vvvv1352@naver.com>
2026-03-16 21:29:48 +0900

    Refactor stock sorting logic to use correct property names

--- lib/stockSort.ts
+++ lib/stockSort.ts
@@ -1,19 +1,15 @@
 import type { StockItem, StockSortKey } from "@/types/stock";
 
-export const sortStocks = (
-  visibleList: StockItem[],
-  key: StockSortKey,
-  isUp: boolean
-) => {
+export const sortStocks = (visibleList: StockItem[], key: StockSortKey, isUp: boolean) => {
   return isUp
     ? [...visibleList].sort((stock1, stock2) => {
-        if (key === "price") return stock1.data.c - stock2.data.c;
-        else if (key === "changeRate") return stock1.data.dp - stock2.data.dp;
+        if (key === "price") return stock1.data.currentPrice - stock2.data.currentPrice;
+        else if (key === "changeRate") return stock1.data.percentChange - stock2.data.percentChange;
         else return 0;
       })
     : [...visibleList].sort((stock1, stock2) => {
-        if (key === "price") return stock2.data.c - stock1.data.c;
-        else if (key === "changeRate") return stock2.data.dp - stock1.data.dp;
+        if (key === "price") return stock2.data.currentPrice - stock1.data.currentPrice;
+        else if (key === "changeRate") return stock2.data.percentChange - stock1.data.percentChange;
         else return 0;
       });
 };
